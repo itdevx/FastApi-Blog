befb439670123f7f1bd194d12eff208006e4bb4f
itdevx <testwebsite1244@gmail.com>
2024-03-28 14:06:06 +0330

    add delete user

--- routers/users.py
+++ routers/users.py
@@ -1,5 +1,5 @@
 from fastapi  import APIRouter, Body, Depends
-from schema._input import RegisterInput
+from schema._input import RegisterInput, UpdateUserProfileInput, DeleteUserAccountInput
 from db.engine import get_db
 from typing import Annotated
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -23,12 +23,18 @@ async def login():
     ...
 
 
-@router.get('/{username}')
+@router.get('/{username}/')
 async def get_user_profile(db_session: Annotated[AsyncSession, Depends(get_db)], username: str):
     user_profile = await UsersOperation(db_session=db_session).get_user_by_username(username=username)
     return user_profile
 
 
-@router.put('/update-profile')
-async def user_update_profile():
-    ...
+@router.put('/update-profile/')
+async def user_update_profile(db_session: Annotated[AsyncSession, Depends(get_db)], data: UpdateUserProfileInput = Body()):
+    user = await UsersOperation(db_session).update_username(old_username=data.old_username, new_username=data.new_username)
+    return user
+
+
+@router.delete('/delete/')
+async def delete_user_account(db_session: Annotated[AsyncSession, Depends(get_db)], data: DeleteUserAccountInput = Body()):
+    await UsersOperation(db_session).delete(username=data.username, password=data.password)
--- schema/_input.py
+++ schema/_input.py
@@ -4,3 +4,13 @@ from pydantic import BaseModel
 class RegisterInput(BaseModel):
     username: str
     password: str
+
+
+class UpdateUserProfileInput(BaseModel):
+    old_username: str
+    new_username: str
+
+
+class DeleteUserAccountInput(BaseModel):
+    username: str
+    password: str
