825c1efc75e03ca08bb99ef1d91501afef326d79
itdevx <testwebsite1244@gmail.com>
2024-03-28 09:50:15 +0330

    add schenas file

--- db/models.py
+++ db/models.py
@@ -1,10 +1,11 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from db.engine import Base
+from uuid import UUID, uuid4
 
 class User(Base):
     __tablename__ = 'users'
 
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    id: Mapped[UUID] = mapped_column(primary_key=True, default_factory=uuid4)
     password: Mapped[str] = mapped_column()
-    username: Mapped[str | None] = mapped_column(unique=True, default=None, nullable=True)
+    username: Mapped[str | None] = mapped_column(unique=True)
 
