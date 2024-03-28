df56592c631511d02b83a32d47d348a74cb2d738
itdevx <testwebsite1244@gmail.com>
2024-03-28 10:13:25 +0330

    add UserOperation with Repository pattern for crud

--- db/models.py
+++ db/models.py
@@ -2,10 +2,10 @@ from sqlalchemy.orm import Mapped, mapped_column
 from db.engine import Base
 from uuid import UUID, uuid4
 
+
 class User(Base):
     __tablename__ = 'users'
 
-    id: Mapped[UUID] = mapped_column(primary_key=True, default_factory=uuid4)
     password: Mapped[str] = mapped_column()
     username: Mapped[str | None] = mapped_column(unique=True)
-
+    id: Mapped[UUID] = mapped_column(primary_key=True, default_factory=uuid4)
