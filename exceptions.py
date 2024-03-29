0df1e2466ad0a5d7a0c2b0335cd363da1333a0a1
itdevx <testwebsite1244@gmail.com>
2024-03-29 18:54:51 +0330

    add username or password incorrect

--- exceptions.py
+++ exceptions.py
@@ -5,4 +5,9 @@ class UserAlreadyExists(HTTPException):
     def __init__(self) -> None:
         self.status_code = 400
         self.detail = 'user already exists!'
-        
+
+
+class UsernameOrPasswordIncorrect(HTTPException):
+    def __init__(self) -> None:
+        self.status_code = 400
+        self.detail = 'username or password is incorrect'
